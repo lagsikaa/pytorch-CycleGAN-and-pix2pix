1f8f8fa4dd167811db69a57adde799ac7d734b92
lagsikaa <158858480+lagsikaa@users.noreply.github.com>
2024-04-25 20:19:24 +0100

    train.py

--- train.py
+++ train.py
@@ -13,21 +13,25 @@ Example:
         python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan
     Train a pix2pix model:
         python train.py --dataroot ./datasets/facades --name facades_pix2pix --model pix2pix --direction BtoA
-
-See options/base_options.py and options/train_options.py for more training options.
-See training and test tips at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/tips.md
-See frequently asked questions at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/qa.md
 """
 import time
 from options.train_options import TrainOptions
 from data import create_dataset
 from models import create_model
 from util.visualizer import Visualizer
+from torch.utils.data import DataLoader
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
-    dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
-    dataset_size = len(dataset)    # get the number of images in the dataset.
+
+    # Create a dataset given opt.dataset_mode and other options
+    train_dataset = create_dataset(opt, train=True)  # You might need to modify the create_dataset function
+    val_dataset = create_dataset(opt, train=False)
+
+    train_loader = DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=opt.batch_size, shuffle=False)
+
+    dataset_size = len(train_loader)    # get the number of batches in the dataset.
     print('The number of training images = %d' % dataset_size)
 
     model = create_model(opt)      # create a model given opt.model and other options
@@ -35,13 +39,14 @@ if __name__ == '__main__':
     visualizer = Visualizer(opt)   # create a visualizer that display/save images and plots
     total_iters = 0                # the total number of training iterations
 
-    for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
+    for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):    
+        model.train()  # Set model to training mode
         epoch_start_time = time.time()  # timer for entire epoch
         iter_data_time = time.time()    # timer for data loading per iteration
         epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
         visualizer.reset()              # reset the visualizer: make sure it saves the results to HTML at least once every epoch
-        model.update_learning_rate()    # update learning rates in the beginning of every epoch.
-        for i, data in enumerate(dataset):  # inner loop within one epoch
+
+        for i, data in enumerate(train_loader):  # inner loop within one epoch
             iter_start_time = time.time()  # timer for computation per iteration
             if total_iters % opt.print_freq == 0:
                 t_data = iter_start_time - iter_data_time
@@ -58,7 +63,7 @@ if __name__ == '__main__':
 
             if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
                 losses = model.get_current_losses()
-                t_comp = (time.time() - iter_start_time) / opt.batch_size
+                accuracy = model.compute_accuracy()  # Compute and log training accuracy
                 visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
                 if opt.display_id > 0:
                     visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
@@ -69,9 +74,22 @@ if __name__ == '__main__':
                 model.save_networks(save_suffix)
 
             iter_data_time = time.time()
+
+        # Validation phase
+        model.eval()  # Set model to evaluation mode
+        with torch.no_grad():
+            val_accuracies = []
+            for i, data in enumerate(val_loader):
+                model.set_input(data)
+                model.test()  # Make sure this doesn't update weights
+                val_accuracies.append(model.compute_accuracy())
+            val_accuracy = sum(val_accuracies) / len(val_accuracies)
+            print(f'Epoch {epoch}, Validation Accuracy: {val_accuracy}')
+
         if epoch % opt.save_epoch_freq == 0:              # cache our model every <save_epoch_freq> epochs
             print('saving the model at the end of epoch %d, iters %d' % (epoch, total_iters))
             model.save_networks('latest')
             model.save_networks(epoch)
 
         print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.n_epochs + opt.n_epochs_decay, time.time() - epoch_start_time))
+
